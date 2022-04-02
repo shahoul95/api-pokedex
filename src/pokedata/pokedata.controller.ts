5dd6028682a9343c29e4ff47aa598384d7f615b7
shahoul95 <66682178+shahoul95@users.noreply.github.com>
2022-04-02 21:24:30 +0200

    refacto(pokedata.controller.ts, pokedata.service.ts): delete getPokemonName and getPokemonAll controller for call api with offset and limit

--- src/pokedata/pokedata.controller.ts
+++ src/pokedata/pokedata.controller.ts
@@ -3,18 +3,8 @@ import { PokedataService } from '../service/pokedata/pokedata.service';
 @Controller('pokedata')
 export class PokedataController {
   constructor(private pokeDataService: PokedataService) {}
-  @Get('pokemon/:name')
-  getPokemonName(@Param('name') name) {
-    return this.pokeDataService.getPokemonName(name);
-  }
-
-  @Get('pokemon/:url')
-  getPokemonUrl(@Param('url') url) {
-    return this.pokeDataService.getPokemonUrl(url);
-  }
-
-  @Get('pokemon')
-  getAllPokemon() {
-    return this.pokeDataService.getAllPokemon();
+  @Get('pokemon/:offset/:limit')
+  getPokemon(@Param('offset') offset: string, @Param('limit') limit: string) {
+    return this.pokeDataService.getPokemonName(offset, limit);
   }
 }
--- src/service/pokedata/pokedata.service.ts
+++ src/service/pokedata/pokedata.service.ts
@@ -2,33 +2,11 @@ import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 @Injectable()
 export class PokedataService {
-  async getPokemonName(name: string) {
+  async getPokemonName(offset: string, limit: string) {
     return await axios
-      .get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
       .then(function (res) {
         return res.data;
-      })
-      .catch(function (error) {
-        throw error;
-      });
-  }
-
-  async getPokemonUrl(url: string) {
-    return await axios
-      .get(url)
-      .then(function (res) {
-        return res.data;
-      })
-      .catch(function (error) {
-        throw error;
-      });
-  }
-
-  async getAllPokemon() {
-    return await axios
-      .get('https://pokeapi.co/api/v2/pokemon?limit=150')
-      .then(function (res) {
-        return res.data.results;
       })
       .catch(function (error) {
         throw error;
