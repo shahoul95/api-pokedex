0a9cdab9d5bc35df1edba9aa24f973a3f587ac8d
shahoul95 <66682178+shahoul95@users.noreply.github.com>
2022-04-02 17:04:23 +0200

    feature(pokedata.controller.ts, service/pokedata.service.ts, pokedamodule.ts, pokedata.resolver.ts): add controller pokedata controller and service resolver with add module pokeda.module.ts to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PokeDataModule } from './items/items.module';
+import { PokedataService } from './pokedata/pokedata.service';
 
 @Module({
-  imports: [],
+  imports: [PokeDataModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PokedataService],
 })
 export class AppModule {}
