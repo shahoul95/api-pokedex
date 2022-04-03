dd4ecd4c8d2739511fbcc25d11b0006886bc335f
shahoul95 <66682178+shahoul95@users.noreply.github.com>
2022-04-03 19:28:12 +0200

    fix(pokedata.module.ts, pokedata.resolver.spec.ts, pokedata.resolver.ts): Delete file resolver pokemon

--- src/pokedata/pokedata.module.ts
+++ src/pokedata/pokedata.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PokedataService } from '../service/pokedata/pokedata.service';
-import { PokedataResolver } from './pokedata.resolver';
 import { PokedataController } from './pokedata.controller';
 
 @Module({
-  providers: [PokedataService, PokedataResolver],
+  providers: [PokedataService],
   controllers: [PokedataController],
 })
 export class PokedataModule {}
