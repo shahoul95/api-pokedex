9d74105b8653180bb82fccaa7054b599f0180c20
shahoul95 <66682178+shahoul95@users.noreply.github.com>
2022-04-10 17:50:28 +0200

    fix(pokemon.controller.ts): Rename PokemonController

--- src/controller/PokemonController/pokemon.controller.ts
+++ src/controller/PokemonController/pokemon.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { PokemonService } from '../../service/pokemon.service/pokemon.service';
-@Controller('pokemon.service')
+@Controller('pokemon')
 export class PokemonController {
   constructor(private pokeDataService: PokemonService) {}
-  @Get('pokemon/:offset/:limit')
+  @Get(':offset/:limit')
   getPokemon(
     @Param('offset', ParseIntPipe) offset: number,
     @Param('limit', ParseIntPipe) limit: number,
